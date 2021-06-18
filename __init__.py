a3f8caeec601856e06001630a7197031c99257b5
Jake <505e06b2@gmail.com>
2021-06-18 21:05:21 +0100

    setBulk working

--- __init__.py
+++ __init__.py
@@ -4,12 +4,15 @@
 #a lot of these functions are not portable to other languages, since they rely on python dicts being in-order
 #would have used urllib, but needed greater control
 
-import requests, base64, random, time, html
+import base64, random, time, html, urllib, json
 
 from .mib import mib_dict, mib_reverse_dict
 
 def parseIP(hex_string):
 	return ".".join([str(int(hex_string[i:i+2], 16)) for i in range(1, len(hex_string), 2)]) #lol pythonic
+
+def encodeIP(ip_string):
+	return "".join(["%02X" % int(x) for x in ip_string.split(".")])
 
 def parseMac(hex_string):
 	return ":".join([hex_string[i:i+2].lower() for i in range(1, len(hex_string), 2)]) #lol pythonic
@@ -38,15 +41,22 @@ class Superhub:
 
 	def get(self, oid):
 		with self._manipulateOid("snmpGet", oid) as r:
-			return r.json()
+			return json.load(r)
 
 	def walk(self, oid):
 		with self._manipulateOid("walk", oid) as r:
-			return r.json()
+			return json.load(r)
 
 	def set(self, oid):
 		with self._generateRequest("snmpSet", {"oid": oid}) as r:
-			return r.json()
+			return json.load(r)
+
+	def setBulk(self, oids):
+		oids_str = ";\\n".join(oids) + ";\\n"# Referer: http://192.168.0.1/?ipfilter&mid=IPFiltering
+		with self._generateRequest("snmpSetBulk", {"oids": oids_str}) as r:
+			ret = r.read()
+		self.set(mib_dict["apply"])
+		return ret
 
 	#General functions
 	def getWiFiPasswords(self):
@@ -58,13 +68,13 @@ class Superhub:
 
 	def getThisMachineNetworkInfo(self):
 		with self._generateRequest("checkConnType") as r:
-			return r.json()
+			return json.load(r)
 
 	def getConnectedDeviceInfo(self, target_ip=None):
 		store = {}
 		ret = {}
 		with self._generateRequest("getConnDevices") as r:
-			result = r.json()
+			result = json.load(r)
 
 		for key, value in result.items():
 			split_ip = key.rsplit(".", 4)
@@ -90,6 +100,30 @@ class Superhub:
 		return ret
 
 	#Port filter functions
+
+	#creates a port filter that blocks all ports for these IPs
+	def createPortFilter(self, src_ip_start, src_ip_end, dst_ip_start, dst_ip_end, block_state=False):
+		addition_index = self.countPortFilters() + 1
+		self.setBulk([
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.2.{addition_index}=1;2",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.3.{addition_index}=1;2",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.4.{addition_index}=3;2",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.5.{addition_index}=2;2",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.6.{addition_index}=%24{encodeIP(src_ip_start) if src_ip_start else 'C0A80000'};4",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.7.{addition_index}=%24{encodeIP(src_ip_end) if src_ip_end else 'C0A80000'};4",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.8.{addition_index}=0;66",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.9.{addition_index}=0;2",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.10.{addition_index}=%24{encodeIP(dst_ip_start) if dst_ip_start else '00000000'};4",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.11.{addition_index}=%24{encodeIP(dst_ip_end) if dst_ip_end else '00000000'};4",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.12.{addition_index}=0;66",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.13.{addition_index}=;66",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.14.{addition_index}=;66",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.15.{addition_index}=;66",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.16.{addition_index}=;66",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.17.{addition_index}=1;2",
+			f"1.3.6.1.4.1.4115.1.20.1.1.4.47.1.1.18.{addition_index}={1 if block_state else 2};2"
+		])
+		return addition_index-1 #filterIndex
 
 	#this only supports a single local IP and not a range
 	def getIndexOfPortFilter(self, ip_address):
@@ -136,8 +170,9 @@ class Superhub:
 		query.append(f"_n={self.n_value}")
 		url = f"http://{self.http_address}/{path}?{'&'.join(query)}"
 		#print(url)
-		auth_cookie = {"credential": self.cookie_string} if self.cookie_string else {}
-		return requests.get(url, cookies=auth_cookie) #status_code will be 401 if not logged in
+		req = urllib.request.Request(url) #status_code will be 401 if not logged in
+		req.add_header("Cookie", self.cookie_string)
+		return urllib.request.urlopen(req)
 
 	def _manipulateOid(self, path, oid):
 		if isinstance(oid, list):
@@ -147,17 +182,19 @@ class Superhub:
 	def _login(self):
 		credentials = base64.urlsafe_b64encode(f"{self.username}:{self.password}".encode()).decode()
 		with self._generateRequest("login", {"arg": credentials}) as r:
-			if r.status_code == 200 and r.content:
-				self.cookie_string = r.content.decode()
-				self.last_login_time = time.time()
-				return True
+			self.cookie_string = "credential=" + r.read().decode("utf-8")
+			self.last_login_time = time.time()
+			return True
 		return False
 
 	def _logout(self):
 		if self.last_login_time != 0:
 			self.last_login_time = 0
-			with self._generateRequest("logout") as r:
-				if r.status_code == 500: #for some reason seems to work with 500
-					self.cookie_string = ""
-					return True
-				return False
+			try:
+				with self._generateRequest("logout") as r:
+					if r.status == 500: #for some reason seems to work with 500
+						self.cookie_string = ""
+						return True
+					return False
+			except urllib.error.HTTPError: #500 causes fail...
+				pass
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+# [Sister Repo](https://github.com/505e06b2/Wireless-Lagswitch)
+
 Check out [alexmartinio's version](https://github.com/alexmartinio/vmsuperhub-smnp)
 
 Clone this repo into your working directory and import it
